c582d9dff40284ef156d3a88b64d212b061212bb
Nikolaj <nikolajweinell@hotmail.com>
2021-11-10 12:17:56 +0100

    Create and start login html and css

--- src/main/java/web/Index.java
+++ src/main/java/web/Index.java
@@ -6,12 +6,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "Index", value = "/")
+@WebServlet(name = "Index", value = "")
 public class Index extends HttpServlet {
 
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/index.jsp");
         dispatcher.forward(request, response);
     }
 
