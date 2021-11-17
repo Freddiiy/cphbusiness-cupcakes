0093132dba55b8f5460c60104e9ed3e15523bc54
Tsukani <34612305+Tsukani@users.noreply.github.com>
2021-11-17 17:15:51 +0100

    Merged cupcake classes

--- src/main/java/model/Cupcake.java
+++ src/main/java/model/Cupcake.java
@@ -1,16 +1,19 @@
 package model;
 
 public class Cupcake {
-    private Integer id;
+    private int id;
     private String name;
     private String desc;
     private String imageURL;
+    private String bottom;
     private int bottomID;
     private int bottomPrice;
+    private String topping;
     private int toppingID;
     private int toppingPrice;
+    private int amount;
 
-    public Cupcake(Integer id, String name, String desc, String imageURL, int bottomID, int bottomPrice, int toppingID, int toppingPrice) {
+    public Cupcake(int id, String name, String desc, String imageURL, int bottomID, int bottomPrice, int toppingID, int toppingPrice) {
         this.id = id;
         this.name = name;
         this.desc = desc;
@@ -21,7 +24,13 @@ public class Cupcake {
         this.toppingPrice = toppingPrice;
     }
 
-    public Integer getId() {
+    public Cupcake(String bottom, String topping, int amount) {
+        this.bottom = bottom;
+        this.topping = topping;
+        this.amount = amount;
+    }
+
+    public int getId() {
         return id;
     }
 
@@ -51,5 +60,17 @@ public class Cupcake {
 
     public int getToppingPrice() {
         return toppingPrice;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getBottom() {
+        return bottom;
+    }
+
+    public String getTopping() {
+        return topping;
     }
 }
